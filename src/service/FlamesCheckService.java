8ee72fb00e4ced17f7f7c84523c3bbb54ab9a306
suyashthonte0 <suyashthonte0@gmail.com>
2020-07-29 18:46:38 +0530

    FSJ0320B013

--- src/service/FlamesCheckService.java
+++ src/service/FlamesCheckService.java
@@ -7,3 +7,38 @@ package service;
 // You must return only the following values ['f','l','a','m','e','s']
 // change the return value at the end of the method corresponding to your return value
 
+public class FlamesCheckService{
+	public char findFlames(String name1, String name2)
+	{
+		int count = 0;
+		char[] name1Array = name1.toCharArray();
+		char[] name2Array = name2.toCharArray();
+		for(int i = 0 ; i < name1Array.length ; i++)
+		{
+			for(int j = 0 ; j < name2Array.length ; j++)
+			{
+				if(name1Array[i] == name2Array[j])
+				{
+					name1Array[i] = name2Array[j] = '1';
+				}
+			}
+		}
+		for(int i = 0 ; i < name1Array.length ; i++)
+		{
+			if(name1Array[i] != '1')
+			{
+				count++;
+			}
+		}
+		for(int j = 0 ; j < name2Array.length ; j++)
+		{
+			if(name2Array[j] != '1')
+			{
+				count++;
+			}
+		}
+		char[] Flames = {'f','l','a','m','e','s'};
+		int k = count % 6;
+		return Flames[k-1];
+	}
+}
